665761e87c24b5f480707bd8b7e51ab504f26616
Ramisa789 <ramisa.islam@ucalgary.ca>
2024-03-11 14:19:07 -0600

    commit

--- lab8_notRepo/products-app/src/components/HomePage.js
+++ lab8_notRepo/products-app/src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Ramisa Islam</td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Farha Ahmed</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
